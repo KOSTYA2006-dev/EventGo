484de6944fe3b53db852ce5df3995e4d491f07b4
Kostya <81483054+KOSTYA2006-dev@users.noreply.github.com>
2026-01-09 23:53:39 +0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>EventGo - Купить билеты</title>
     <link rel="stylesheet" href="{{ asset('assets/css/main-set.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/main-page.css') }}">
